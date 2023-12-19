fc08fcd0a7af316c154276869c5bbdecd5963a0f
D14rn <49399494+D14rn@users.noreply.github.com>
2023-12-19 16:11:00 +0100

    Fix bugs in error managing related classes

--- main/src/lib/http_error_manager.php
+++ main/src/lib/http_error_manager.php
@@ -1,6 +1,7 @@
 <?php
 
 class HttpErrorManager {
+    private static string $defaultErrorCode = "500";
     private static array $http_error_codes = [
         "401" => "Unauthorized",
         "404" => "Not Found",
@@ -8,26 +9,24 @@ class HttpErrorManager {
     ];
 
 # Function to get a valid error message
-    public static function get_error_message(string $error_code) {
-        return self::$http_error_codes[sanitize_error_code($error_code)];
+    public static function getErrorMessage(string $error_code) {
+        return self::$http_error_codes[self::sanitizeErrorCode($error_code)];
     }
 
     # Method to verify an error code is valid
     public static function isValidErrorCode(string $error_code) {
-        echo "these are the error codes ";
-        var_dump($http_error_codes);
-        if (key_exists($error_code, $http_error_codes)) {
+        if (key_exists($error_code, self::$http_error_codes)) {
             return true;
         }
         return false;
     }
 
     # Method to make sure we return a valid error code
-    public static function sanitizeErrorCode(string $error_code="500") {
+    public static function sanitizeErrorCode(string $error_code) {
         if (self::isValidErrorCode($error_code)) {
             return $error_code;
         }
-        return "500";
+        return self::$defaultErrorCode;
     }
 }
 
