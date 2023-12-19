d7f12f0a77714fe99cc4daac4aa966ff6db24bee
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-19 14:27:28 +0100

    some new comment on Model class

--- main/src/core/model.php
+++ main/src/core/model.php
@@ -9,9 +9,11 @@ abstract class Model {
         # Lazy loading to delay database connection until time of request (we don't want to connect if we don't make a request)
         self::setDatabase();
         if ($params === null) {
+            # if the request has no parameters, we do it with the query method.
             $res = self::getDatabase()->query($sql);
         }
         else {
+            # But, if the request has parameters, we would do a prepared request to avoid SQL injection.
             $res = self::getDatabase()->prepare($sql);
             $res->execute($params);
         }
@@ -20,7 +22,7 @@ abstract class Model {
     
     private static function setDatabase() {
         if (self::$db === null) {
-            # If db is not set, initialize it
+            # If db is not set, initialize it with the configuration
             $dsn = Configuration::get("dsn");
             $username = Configuration::get("user");
             $password = Configuration::get("password");
