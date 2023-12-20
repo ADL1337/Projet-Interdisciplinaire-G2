443734384a3903a3c58e74baca6a3ff454faf64a
fabiomirasola <mirasolafabio@hotmail.com>
2023-12-20 10:59:13 +0100

    LoginController progress

--- main/src/controllers/LoginController.php
+++ main/src/controllers/LoginController.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../core/controller.php';
 require_once __DIR__ . '/../views/login.php';
 require_once __DIR__ . '/../core/view.php';
+require_once __DIR__ . '/../lib/HttpErrorManager.php';
 
 class LoginController extends Controller{
     public function control_user(int $user_id, string $user_email, string $user_password){
@@ -14,9 +15,9 @@ class LoginController extends Controller{
         $view=new View('login', 'Login');
         $generate=$view->generateView([
             '$test'=>'salut']);
-            if ((self:: == "POST") && (isset($_POST['user_email'])) && isset($_POST['user_password'])){
-                if ($_POST['user_email'] == true && $_POST['user_password'] == true){
-
+            if ((self::$requestMethod == "POST") && (isset(self::postAttribute('user_email'))) && isset(self::postAttribute('user_password'))){
+                if ($_POST['user_email'] == false && $_POST['user_password'] == false){
+                    self::redirectError();
 
                 }
         }
