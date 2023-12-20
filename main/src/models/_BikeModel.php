890f79bc993e79bf1197db2a6db55c339a5b56b7
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-20 12:07:28 +0100

    add page for bike type list

--- main/src/models/_BikeModel.php
+++ main/src/models/_BikeModel.php
@@ -6,4 +6,9 @@ class BikeModel extends Model {
         $sql = "SELECT `bike`.`bike_id`, `bike`.`bike_color`, `bike`.`bike_size`, `type`.`type_name` from bike INNER JOIN `type` on `type`.`type_id` = bike.bike_type;";
         return self::executeRequest($sql);
     }
+
+    public static function getBikeTypes(){
+        $sql = "SELECT `type`.`type_name`, `type`.`type_id` FROM `type`";
+        return self::executeRequest($sql);
+    }
 }
