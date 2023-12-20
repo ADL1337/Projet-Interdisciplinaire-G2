303be8f9ff15c0841d9b264cc4e97858053ce4c6
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-20 08:39:53 +0100

    add Bike list

---
+++ main/src/controllers/AdminBikeListController.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . "/../core/controller.php";
+require_once __DIR__ . "/../core/view.php";
+require_once __DIR__ . "/../models/_BikeModel.php";
+
+class AdminBikeListController extends Controller {
+    public static function execute(){ 
+        $bikes = BikeModel::getBikes();
+        ob_start();
+        ?>
+        <table>
+            <?php while($bike = $bikes->fetch()){
+                ?>
+                <tr>
+                    <td><?= $bike["type_name"] ?></td>
+                    <td><?= $bike["bike_color"] ?></td>
+                    <td><?= $bike["bike_size"] ?></td>
+                </tr>
+                <?php
+            } ?>
+        </table>
+        <?php
+        $listBike = ob_get_clean();
+        $view = new View("bike_list_preview", "Bike management");
+        $test = $view->generateView([
+            "listBike" => $listBike,
+        ]);
+        echo $test;
+    }
+    public static function g(){}
+}
+
+?>
---
+++ main/src/models/_BikeModel.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . "/../core/model.php";
+
+class BikeModel extends Model {
+    public static function getBikes(){
+        $sql = "SELECT `bike`.`bike_id`, `bike`.`bike_color`, `bike`.`bike_size`, `type`.`type_name` from bike INNER JOIN `type` on `type`.`type_id` = bike.bike_type;";
+        return self::executeRequest($sql);
+    }
+}
