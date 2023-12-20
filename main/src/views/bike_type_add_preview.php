b70a90ae41d9dfd7b1b10326a09c14ec4bf3c351
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-20 12:08:15 +0100

    add bike type add page

---
+++ main/src/views/bike_type_add_preview.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <label for="bike_label">The label of the type to add :</label>
+    <input type="text" id="bike_label" name="bike_label" placeholder="The label of the type">
+    <input type="submit" value="Add">
+</form>
