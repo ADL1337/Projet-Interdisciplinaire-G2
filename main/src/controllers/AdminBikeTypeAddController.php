cc7c219cbe82a8367a16b41a7d459f2bbc3acb5f
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-20 13:30:28 +0100

    fix merge

--- main/src/controllers/AdminBikeTypeAddController.php
+++ main/src/controllers/AdminBikeTypeAddController.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/../models/_BikeModel.php";
 
 class AdminBikeTypeAddController extends Controller {
     public static function execute(){
-        $typeLabel = self::postAttribute("bike_label");
+        $typeLabel = self::fetchPOST("bike_label");
         if(isset($typeLabel) && !empty($typeLabel) && preg_match("/^[a-zA-Zâàêéèëïîï\ ]{1,}$/", $typeLabel) == 1){
             $res = BikeModel::addBikeType($typeLabel);
             if($res->rowCount() == 1){
