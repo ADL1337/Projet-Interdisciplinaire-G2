a54d04d4c42db97f80cf927d2ec65f1d4a33430d
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-20 17:09:02 +0100

    update bike add controller for the form handling

--- main/src/controllers/AdminBikeAddController.php
+++ main/src/controllers/AdminBikeAddController.php
@@ -2,9 +2,27 @@
 require_once __DIR__ . "/../core/controller.php";
 require_once __DIR__ . "/../core/view.php";
 require_once __DIR__ . "/../models/_BikeModel.php";
+require_once __DIR__ . "/ErrorController.php";
 
 class AdminBikeAddController extends Controller {
-    public static function execute(){ 
+    public static function execute(){
+        if(self::isPostRequest() && self::isSetPOST("bike_type") && self::isSetPOST("size") && self::isSetPOST("color")){
+            $type = intval(self::fetchPOST("bike_type"));
+            $size = intval(self::fetchPOST("size"));
+            $color = self::fetchPOST("color");
+            if(is_int($type) && is_int($size) && preg_match("/^[a-zA-Zâàêéèëïîï\ ]{1,}$/", $color) == 1){
+                $res = BikeModel::addBike($type, $color, $size);
+                if($res == false){
+                    ErrorController::setError(500);
+                    exit;
+                } else {
+                    header('Location: /listBikeType');
+                    exit;
+                }
+            }
+            ErrorController::setError(500);
+            exit;
+        }
         $bikeTypes = BikeModel::getBikeTypes();
         ob_start();
         while($bikeType = $bikeTypes->fetch()){
