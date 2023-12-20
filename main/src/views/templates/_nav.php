11b0cb5382d1045186f4eed2099d7efd8df9615f
ADL1337 <arnaud.2.lena@gmail.com>
2023-12-20 16:33:25 +0100

    Add middleware for admin and user verification and also create nav template and add some error codes to errormanager

---
+++ main/src/views/templates/_nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul class="flex-space-between">
+        <li class="nav-logo"></li>
+        <li class="nav-title">ISIMS PARK</li>
+        <ul class="nav-user_info">
+            <li class="nav-firstname">Welcome <?= $firstname ?></li>
+            <li class="nav-logout"><a href="logout.php">LOG OUT</a>
+        </ul>
+    </ul>
+</nav>
