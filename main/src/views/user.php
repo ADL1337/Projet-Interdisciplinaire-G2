0052ae8bad4c5a8e9eabed117f99ebace6037ac5
shadowthib <71013397+shadowthib@users.noreply.github.com>
2023-12-21 16:24:42 +0100

    update

--- main/src/views/user.php
+++ main/src/views/user.php
@@ -1,5 +1,11 @@
-<ul>
-<li id="reservation"><a href="/reservation">Reservation</a></li>
-<li><a href="/reservationList">See my reservations</a></li>
-<li><a href="/repair">Repair my bike</a></li>
-</ul>
+<div id="user">
+    <div class="button_user" id="reservation">
+        <a href="/reservation">Reservation</a>      
+    </div>
+    <div class="button_user">
+            <a href="/reservationList">See my reservations</a> 
+    </div>
+    <div class="button_user">
+            <a href="/repair">Repair my bike</a>
+    </div>
+</div>
