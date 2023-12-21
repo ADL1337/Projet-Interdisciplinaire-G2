b3978ec6c6921f3fc4c4c7754c19354b5a5bcaf0
TiasMat16 <mattias.bruneau@std.heh.be>
2023-12-21 11:43:35 +0100

    front-end : navbar & login_page

--- main/src/views/login.php
+++ main/src/views/login.php
@@ -1,9 +1,20 @@
-<form method="POST" action="login">
-    <label for="email">Email</label><br>
-    <input type="email" name="user_email"><br>
-
-    <label for="password">Password</label><br>
-    <input type="password" name="user_password" ><br>
-
-    <input type="submit" value="Log In">
-</form>
+<div class ="form-container">
+    <div class="form">
+        <h2>Log In</h2>
+        <form method="POST" action="login">
+            <div class="mt-5-mb-5">
+                <label for="email">Email</label><br>
+                <input type="email" name="user_email"><br>
+            </div>
+            <div class="mt-5-mb-5">
+                <label for="password">Password</label><br>
+                <input type="password" name="user_password" ><br>
+            </div>
+            <div class="mt-5-mb-5">
+                <div class="submit-button">
+                    <input type="submit" value="Log In">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
