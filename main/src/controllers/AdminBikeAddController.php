3bc015f5e9be443c7f9d2939820c2de6207eccb0
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-21 21:25:07 +0100

    add comments to make the code more understandable

--- main/src/controllers/AdminBikeAddController.php
+++ main/src/controllers/AdminBikeAddController.php
@@ -4,22 +4,33 @@ require_once __DIR__ . "/../models/_BikeModel.php";
 require_once __DIR__ . "/../lib/HttpErrorManager.php";
 require_once __DIR__ . "/../lib/PrivilegeMiddleware.php";
 
+# Controller for the admin's page to add bike
 class AdminBikeAddController extends Controller {
     public static function execute(){
+        # check if the user is logged in and if he's an admin user
         PrivilegeMiddleware::requireAdmin();
+
+        # if it's a post request and all the fields are settled
         if(self::isPostRequest() && self::isSetPOST("bike_type") && self::isSetPOST("size") && self::isSetPOST("color")){
             $type = intval(self::fetchPOST("bike_type"));
             $size = intval(self::fetchPOST("size"));
             $color = self::fetchPOST("color");
+
+            # check if all the value are in the correct type and format
             if(is_int($type) && is_int($size) && preg_match("/^[a-zA-Zâàêéèëïîï\ ]{1,}$/", $color) == 1){
                 $res = BikeModel::addBike($type, $color, $size);
+
+                # if the insert is unsuccessful we redirect to the error page
                 if($res == false){
                     HttpErrorManager::redirectError("500");
+                # But if the insert is successful, we redirect to the bike list
                 } else {
                     header('Location: /listBikeType');
                     exit();
                 }
             }
+
+            # if he didn't got redirected it means the request is not correct
             HttpErrorManager::redirectError("500");
         }
         $bikeTypes = BikeModel::getBikeTypes();
@@ -30,6 +41,8 @@ class AdminBikeAddController extends Controller {
             <?php
         }
         $listBikeTypes = ob_get_clean();
+
+        # we generate the view and we show it
         $view = new View("bike_add_preview", "Add one bike");
         $generatedView = $view->generateView([
             "listBikeTypes" => $listBikeTypes,
--- main/src/controllers/UserController.php
+++ main/src/controllers/UserController.php
@@ -1,9 +1,12 @@
 <?php
 require_once __DIR__ . "/../core/controller.php";
 
+# Controller for the main user's page
 class UserController extends Controller {
     public static function execute(){
+        # check if the user is logged in as a user
         PrivilegeMiddleware::requireUser();
+        # render the user main page
         $view = new View("user", "Isims Park");
         $generatedView = $view->generateView([]);
         echo $generatedView;
