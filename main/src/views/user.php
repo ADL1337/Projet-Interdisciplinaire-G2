cf90012f1cc089b533594eb3b132f2a4f8889aad
shadowthib <71013397+shadowthib@users.noreply.github.com>
2023-12-21 16:02:18 +0100

    Update
    Coloriage user

--- main/src/views/user.php
+++ main/src/views/user.php
@@ -1,3 +1,5 @@
-<a href="/reservation">Reservation</a>
-<a href="/reservationList">See my reservations</a>
-<a href="/repair">Repair my bike</a>
+<ul>
+<li id="reservation"><a href="/reservation">Reservation</a></li>
+<li><a href="/reservationList">See my reservations</a></li>
+<li><a href="/repair">Repair my bike</a></li>
+</ul>
