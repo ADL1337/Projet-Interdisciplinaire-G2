221a1d01c4aaa0409e27cdbd128c0c29373c5797
TiasMat16 <mattias.bruneau@std.heh.be>
2023-12-21 11:43:35 +0100

    front-end : navbar & login_page

--- main/src/views/templates/_nav.php
+++ main/src/views/templates/_nav.php
@@ -1,8 +1,8 @@
 <header>
     <nav>
         <div class="flex-space-between">
-            <div class="nav-logo"></div>
-            <div class="nav-title">ISIMS PARK</div>
+            <div class="nav-logo"><img src="/res/img/nav_icon_velo.svg"></img></div>
+            <h1 class="nav-title">ISIMS PARK</h1>
             <div class="nav-user_info">
             <?php if (isset($_SESSION["user_firstname"])): ?>
                 <div class="nav-firstname">Welcome <?= $_SESSION["user_firstname"] ?></div>
