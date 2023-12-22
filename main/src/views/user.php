458697288a6d8c8df451061bf20815ba34a7dea0
TiasMat16 <mattias.bruneau@std.heh.be>
2023-12-22 10:31:49 +0100

    hotfixes-change

--- main/src/views/user.php
+++ main/src/views/user.php
@@ -1,15 +1,16 @@
-
-<div id="user">
-    <div class="button_user" >
-        <a href="/reservation">Reservation</a>      
-    </div>
-    <div class="button_user">
+<div class="user-dashboard-container">
+    <h2>User Dashboard</h2>
+    <div class="user-dashboard-container-wrapped">
+        <div class="button_user" >
+            <a href="/reservation">Reservation</a>      
+        </div>
+        <div class="button_user">
             <a href="/reservationList">See my reservations</a> 
-    </div>
-    <div class="button_user">
+        </div>
+        <div class="button_user">
             <a href="/repair">Repair my bike</a>
+        </div>
     </div>
-</div>
 <footer class="user">
     <div class="return">
         <a href ="/login">Return to Dashboard</a>
