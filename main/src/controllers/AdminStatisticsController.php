7fa69ace0f3f4c74bd07decc9f69dd9a7894395f
D14rn <49399494+D14rn@users.noreply.github.com>
2023-12-22 11:49:04 +0100

    require admin on stat page

--- main/src/controllers/AdminStatisticsController.php
+++ main/src/controllers/AdminStatisticsController.php
@@ -4,6 +4,8 @@ require_once __DIR__ . "/../models/StatisticsModel.php";
 
 class AdminStatisticsController extends Controller {
     public static function execute() {
+        PrivilegeMiddleware::requireAdmin();
+
         $totalReservations = StatisticsModel::getTotalReservations(); # total reservations
         $mostReservedBike = StatisticsModel::getMostReservedBike(); # Bike ID
         
