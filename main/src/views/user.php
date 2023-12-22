1cf0d957a870c5b81527155a328bab705da41aaa
TiasMat16 <mattias.bruneau@std.heh.be>
2023-12-22 09:52:25 +0100

    return-to-dashboard

--- main/src/views/user.php
+++ main/src/views/user.php
@@ -1,3 +1,6 @@
 <a href="/reservation">Reservation</a>
 <a href="/reservationList">See my reservations</a>
 <a href="/repair">Repair my bike</a>
+<div class="return">
+    <a href ="/login">Return to Dashboard</a>
+</div>
--- main/src/views/user_reservation.php
+++ main/src/views/user_reservation.php
@@ -1,3 +1,4 @@
 <table>
     <?= $reservations ?>
 </table>
+<a href ="/login">Return to login</a>
