04ffba23a9e75686fdd5c4ad45a16507416ddaf9
D14rn <49399494+D14rn@users.noreply.github.com>
2023-12-22 11:42:19 +0100

    Fix Statistics

--- main/src/models/StatisticsModel.php
+++ main/src/models/StatisticsModel.php
@@ -13,17 +13,15 @@ class StatisticsModel extends Model {
     }
 
     public static function getMostReservedBike() {
-        $sql = 'SELECT bike_id, COUNT(*) as bike_usage_count
+        $sql = 'SELECT COUNT(*) as bike_usage_count
                 FROM reservation
-                GROUP BY bike_id
                 ORDER BY COUNT(*) DESC
                 LIMIT 1';
         $response = self::executeRequest($sql);
         if ($response->rowCount() !== 1) {
             return false;
         }
-        $res = $response->fetch();
-        return [$res["bike_id"], $res["bike_usage_count"]];
+        return $response->fetch()["bike_usage_count"];
     }
 
     /*public static function getMostReservedBikeType() {
