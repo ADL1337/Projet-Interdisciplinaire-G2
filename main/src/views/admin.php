6e3ab42513aab165bef9218bf9e46d2193bbe550
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-22 10:56:42 +0100

    HOTFIX - remove link

--- main/src/views/admin.php
+++ main/src/views/admin.php
@@ -1,5 +1,4 @@
 <h2>Administration</h2>
 <a href="/manageSchedule">Schedule</a>
-<a href="/reservationConfirmation">Confirm a reservation</a>
 <a href="/statistics">Statistics</a>
 <a href="/bikeManagement">Bike Management</a>
