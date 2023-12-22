b387155443be5b5a1e6e0cd42e62395b7e78b060
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-22 11:39:48 +0100

    WIP - debug

--- main/src/models/_RepairModel.php
+++ main/src/models/_RepairModel.php
@@ -3,6 +3,7 @@ require_once __DIR__ . "/../core/model.php";
 
 class RepairModel extends Model {
     public static function repair($id){
+        return;
         $sql = "";
         self::executeRequest($sql, [$id]);
     }
