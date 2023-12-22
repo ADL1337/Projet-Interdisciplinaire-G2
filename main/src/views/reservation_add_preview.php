d5f70e71725a4b87e65f59dfe68a7663773cd6fb
uiuytb <58974078+uiuytb@users.noreply.github.com>
2023-12-22 11:29:04 +0100

    HOTFIX

--- main/src/views/reservation_add_preview.php
+++ main/src/views/reservation_add_preview.php
@@ -2,7 +2,7 @@
     <label for="date_from">Date of the start of the reservation : </label>
     <input type="date" name="date_from" id="date_from" min="<?= $today ?>" value="<?= $today ?>">
     <label for="date_end">Date of the end of the reservation</label>
-    <input type="date" name="date_end" id="date_end" value="<?= $today ?>">
+    <input type="date" name="date_end" min="<?= $today ?>"  id="date_end" value="<?= $today ?>">
     <input type="submit" value="Changer la date de réservation">
 </form>
 <?php if(!is_null($bike)){ ?>
