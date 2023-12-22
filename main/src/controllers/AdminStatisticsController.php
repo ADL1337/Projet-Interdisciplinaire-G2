1257c4b387ba15d81cf27c190c9b860082d1ca39
D14rn <49399494+D14rn@users.noreply.github.com>
2023-12-22 11:23:25 +0100

    Add statistics view

--- main/src/controllers/AdminStatisticsController.php
+++ main/src/controllers/AdminStatisticsController.php
@@ -4,22 +4,21 @@ require_once __DIR__ . "/../models/StatisticsModel.php";
 
 class AdminStatisticsController extends Controller {
     public static function execute() {
-        $totalReservations = StatisticsModel::getTotalReservations();
-        $mostReservedBike = StatisticsModel::getMostReservedBike();
-        #$mostReservedBikeType = StatisticsModel::getMostReservedBikeType();
+        $totalReservations = StatisticsModel::getTotalReservations(); # total reservations
+        $mostReservedBike = StatisticsModel::getMostReservedBike(); # array with ["bike_id" and "bike_usage_count"]
+        $mostReservedBikeID = $mostReservedBike["bike_id"];
+        $mostReservedBikeUsageCount = $mostReservedBike["bike_usage_count"];
+        
+        $totalBikesReserved = "Total bikes reserved: $totalReservations";
+        $mostPopularBike = "Most popular bike: $mostReservedBikeID with $mostReservedBikeUsageCount reservations";
 
-            echo "total reservations: ";
-            var_dump($totalReservations);
-            echo "<br>";
+        $view = new View("statistics", "Statistics");
+        $generatedView = $view->generateView([
+            "totalBikesReserved" => $totalBikesReserved,
+            "mostPopularBike" => $mostPopularBike
+        ]);
 
-            echo "most popular bike ID: ";
-            var_dump($mostReservedBike);
-            echo "<br>";
-        /*while ($res = $mostReservedBikeType->fetch()) {
-            echo "most popular bike type: ";
-            var_dump($res);
-            echo "<br>";
-        }*/
+        echo $generatedView;
     }
 }
 
